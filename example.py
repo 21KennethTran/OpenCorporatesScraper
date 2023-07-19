f703753cddcc7f0f4e6959796404259adbe1a319
Kenneth Tran <kenpt03@gmail.com>
2023-07-19 14:12:40 -0700

    reading javascript file for generating cookies

---
+++ example.py
@@ -0,0 +1,62 @@
+# NOT MINE
+import httpx
+import trio
+from selectolax.lexbor import LexborHTMLParser
+import re
+import js2py
+from pprint import pp
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0'
+}
+
+
+async def get_soup(content):
+    return LexborHTMLParser(content)
+
+
+async def get_company(channel):
+    async with channel:
+        async for client, url in channel:
+            r = await client.get(url)
+            soup = await get_soup(r.content)
+            pp({
+                'Name': soup.css_first('title').text(strip=True).split(':', 1)[0],
+                'Address': soup.css_first('ul.address_lines').text(separator=', '),
+                'Status': soup.css_first('dd.status').text(strip=True)
+            })
+
+
+async def main():
+    async with httpx.AsyncClient(headers=headers, follow_redirects=True, base_url='https://opencorporates.com') as client, trio.open_nursery() as nurse:
+        params = {
+            "action": "search_companies",
+            "commit": "Go",
+            "controller": "searches",
+            "jurisdiction_code": "",
+            "order": "",
+            "q": "RI DUDDING LIMITED",
+            "utf8": "✓"
+        }
+        r = await client.get('/companies', params=params)
+        match = re.search('(function le.*\");', r.text,
+                          re.DOTALL).group(1).replace('{ document.cookie=', 'return ') + "};go()"
+        cookies = js2py.eval_js(match)
+        client.cookies.update({
+            'KEY': cookies.split('=', 1)[1]
+        })
+        r = await client.get(r.url)
+        soup = await get_soup(r.content)
+        comanies = soup.css('a.company_search_result')
+        sender, receiver = trio.open_memory_channel(0)
+        async with receiver:
+            for _ in range(3):
+                nurse.start_soon(get_company, receiver.clone())
+
+            async with sender:
+                for co in comanies:
+                    await sender.send([client, co.attrs['href']])
+
+
+if __name__ == "__main__":
+    trio.run(main)
--- methods.py
+++ methods.py
@@ -4,15 +4,33 @@ from bs4 import BeautifulSoup
 import json
 import pandas as pd
 import datetime
+from pyppeteer import launch
+import asyncio
 
 def getCompanyInfo(company="Crixus Capital", type="companies"):
   company = company.replace(" ","+")
-  url = f"https://opencorporates.com/{type}?q={company}&jurisdiction_code=&type={type}"
+#   url = f"https://opencorporates.com/{type}?q={company}&jurisdiction_code=&type={type}"
+  url = 'https://httpbin.org/headers'
+  header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47',
+            'Accept-Language': 'en-GB,en;q=0.5',
+            'Referer': 'https://google.com',
+            'DNT': '1'}
   
-  header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47'}
   response = requests.get(url, headers=header)
-  # print(response.text)
+  print(response.text)
   
   soup = BeautifulSoup(response.content, 'html.parser')
   print(soup.prettify())
+
+async def company(company="Crixus Capital", type="companies"):
+  browser = await launch()
+  page = await browser.newPage()
+  await page.goto(f"https://opencorporates.com/{type}?q={company}&jurisdiction_code=&type={type}")
+  await page.waitForSelector('title', {'visible': True})
+  await page.screenshot({'path': 'ss2.png'})
+  await browser.close()
+
+  element = await page.querySelector('h1')
+
 getCompanyInfo()
+# asyncio.get_event_loop().run_until_complete(company())
