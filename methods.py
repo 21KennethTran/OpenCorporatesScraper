e76dceb10fb8d7fbb60c0291e96318956d905b0e
Kenneth Tran <kenpt03@gmail.com>
2023-07-19 20:45:12 -0700

    company function complete

--- methods.py
+++ methods.py
@@ -15,10 +15,41 @@ header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWe
 def get_soup(content):
   return BeautifulSoup(content, 'html.parser')
 
-def extract(url):
-  response = requests.get(url, headers=header)
+def extract(url, session):
+  response = requests.get(url, headers=header, cookies=session)
   soup = get_soup(response.content)
-  # TODO: how to parse this
+#   print(soup.prettify())
+
+#   print(soup.find('dd', class_='registry_page').text)
+#   bleh = soup.find('dd', class_='registry_page').find('a')['href']
+#   print(bleh)
+
+
+  filings = []
+  for file in soup.find_all('a', class_='filing'):
+    filing = file.text + ', ' + 'https://opencorporates.com' + file['href']
+    filings.append(filing)
+  
+  latestevents = []
+  for event in soup.find_all('div', class_='event-timeline-row'):
+    latestevent = event.find('dd').find('a').text + ' on ' + event.find('dt').text
+    latestevents.append(latestevent)
+
+  data = {
+    'Company Name': [soup.find('title').text.split(' :')[0]],
+    'Company Number': [soup.find('dd', class_='company_number').text] if soup.find('dd', class_='company_number') else [''],
+    'Status': [soup.find('dd', class_='status').text] if soup.find('dd', class_='status') else [''],
+    'Incorporation Date': [soup.find('dd', class_='incorporation_date').text] if soup.find('dd', class_='incorporation_date') else [''],
+    'Dissolution Date': [soup.find('dd', class_='dissolution date').text] if soup.find('dd', class_='dissolution data') else [''],
+    'Company Type': [soup.find('dd', class_='company_type').text] if soup.find('dd', class_='company_type') else [''],
+    'Jurisdiction': [soup.find('dd', class_='jurisdiction').text] if soup.find('dd', class_='jurisdiction') else [''],
+    'Business Number': [soup.find('dd', class_='business_number').text.strip()] if soup.find('dd', class_='business_number') else [''],
+    'Registry Page': [soup.find('dd', class_='registry_page').find('a')['href']] if soup.find('dd', class_='registry_page') and soup.find('dd', class_='registry_page').find('a') else [''],
+    'Recent filings (data and link)': filings,
+    'Source': [soup.find('span', class_='publisher').text] if soup.find('span', class_='publisher') else [''],
+    'Latest Events (date and description)': latestevents,
+  }
+  return data
 
 def getCompanyInfo(company="Crixus Capital", type="companies"):
   company = company.replace(" ","+")
@@ -39,8 +70,22 @@ def getCompanyInfo(company="Crixus Capital", type="companies"):
     soup = get_soup(new_r.content)
     
     company = soup.find_all('a', class_=['company_search_result inactive deregistered', 'company_search_result'])[0]['href']
-    # print(company)
-    extract('https://opencorporates.com' + company)
+    data = extract('https://opencorporates.com' + company, cookies)
+    dlength = max(len(values) for values in data.values())
+    for key, values in data.items():
+      # remove \n from text we want
+      newval = []
+      for value in values:
+        val = re.sub(r'\n', '', value)
+        newval.append(val)
+      data[key] = newval + ['']*(dlength - len(values))
+
+    # transposed dataframe more intuitive for reading single company info?
+    # maybe json format is better
+    dataframe = pd.DataFrame(data)
+    print(dataframe)
+
+    dataframe.to_csv('companies_data.csv', index=False)
 
 
 # async def company(company="Crixus Capital", type="companies"):
